32633376d5defd16e27e1fb8faca3ea0ce86fd0b
Stephen <slulego@live.com>
2023-09-07 08:23:55 -0400

    fix scrollbar

--- Example/ListView.swift
+++ Example/ListView.swift
@@ -1,5 +1,5 @@
 //
-//  PagerScrollView.swift
+//  ListView.swift
 //  Example
 //
 //  Created by Stephen Ullom on 8/27/23.
@@ -14,31 +14,34 @@ class ListView: UIViewController {
     super.viewDidLoad()
     
     // View
-    view.backgroundColor = .systemRed
+    view.backgroundColor = .systemMint
     
     // ScrollView
     view.addSubview(scrollView)
     scrollView.translatesAutoresizingMaskIntoConstraints = false
+    scrollView.backgroundColor = .systemCyan
     
     // StackView
     let stackView = UIStackView()
     stackView.axis = .vertical
+    stackView.backgroundColor = .systemPink
     stackView.spacing = 24
     stackView.translatesAutoresizingMaskIntoConstraints = false
     scrollView.addSubview(stackView)
     
     NSLayoutConstraint.activate([
-      scrollView.topAnchor.constraint(equalTo: view.topAnchor),
-      scrollView.leftAnchor.constraint(equalTo: view.leftAnchor),
-      scrollView.rightAnchor.constraint(equalTo: view.rightAnchor),
-      scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-      
+      scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      scrollView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+      scrollView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+      scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+
       stackView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 20),
       stackView.leftAnchor.constraint(equalTo: scrollView.leftAnchor, constant: 20),
       stackView.rightAnchor.constraint(equalTo: scrollView.rightAnchor, constant: -20),
       stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -20),
     ])
     
+    // Labels
     for i in 0 ..< 24 {
       let label = UILabel()
       label.text = "Label \(i + 1)"
